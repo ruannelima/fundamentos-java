46afefe34a0df9340f09297bafcfd38e3bf2713f
ruannelima <dev.ruanne@gmail.com>
2026-09-16 23:53:46 -0300

    Adiciona exercício sobre variáveis

---
+++ 02-variaveis/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nome = "Ruanne";
+        int idade = 25;
+        String curso = "Análise e Desenvolvimento de Sistemas";
+        String cidade = "Rio de Janeiro";
+
+        System.out.println("Olá, meu nome é " + nome);
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Estou cursando " + curso);
+        System.out.println("Moro no " + cidade);
+
+    }
+}
