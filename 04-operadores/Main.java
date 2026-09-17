1b734e82e936c13e326128778d812e6921e42110
ruannelima <dev.ruanne@gmail.com>
2026-09-17 17:54:14 -0300

    Adiciona exercício sobre operadores matemáticos

---
+++ 04-operadores/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[]args) {
+
+		int numero01; 
+		int numero02;
+		
+		numero01 = 20;
+		numero02 = 6;
+		
+		int Soma = numero01 + numero02;
+		int Subtracao = numero01 - numero02;
+		int Multiplicacao = numero01 * numero02;
+		int Divisao = numero01 / numero02;
+		int Resto = numero01 % numero02;
+		
+		System.out.println("Soma: " + Soma);
+		System.out.println("Subtração: " + Subtracao);
+		System.out.println("Multiplicação: " + Multiplicacao);
+		System.out.println("Divisão: " + Divisao);
+		System.out.println("Resto: " + Resto);
+		
+		
+ }
+}
