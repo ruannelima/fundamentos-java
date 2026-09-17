d5873ead19e42f5f86a19e312ac757d0c8115782
ruannelima <dev.ruanne@gmail.com>
2026-09-17 19:35:42 -0300

    Adiciona exercício de estruturas condicionais

---
+++ 06-estruturas-condicionais/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double nota;
+		nota = 6.5;
+		
+		if(nota>=7) {
+			System.out.println("Aprovado");
+		}
+		else if(nota>=5) {
+			System.out.println("Recuperação");
+		}
+		else {
+				System.out.println("Reprovado");
+		}
+
+	}
+
+}
