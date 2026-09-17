7d4b3383158632f1296b6c951376a081236bfe84
ruannelima <dev.ruanne@gmail.com>
2026-09-17 17:26:38 -0300

    Adiciona exercício de entrada de dados

---
+++ 03-entrada-de-dados/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome:");
+		String nome = scanner.nextLine();
+		
+		System.out.println("Digite sua idade:");
+		int idade  = scanner.nextInt();
+		
+		System.out.println("Olá, " + nome + "!");
+		System.out.println("Você tem " + idade + " anos.");
+
+	}
+
+}
