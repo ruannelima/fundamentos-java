a714204fa8bb645400cbcc2266ed2da33e7ce4d4
ruannelima <dev.ruanne@gmail.com>
2026-09-19 07:31:24 -0300

    Adiciona exercício da estrutura de repetição while

---
+++ 08-while/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		int numero = 2;
+				
+		while(numero<=10) {
+			
+			System.out.println(numero);
+			numero += 2;
+		} 
+	}
+}
